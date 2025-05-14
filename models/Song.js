451880c8269e646c98532d5c8afafe104261d661
Emmanuel Luis <Emmanuel.luis@verizon.com>
2025-05-14 12:01:49 -0500

    Added Song Model

--- models/Song.js
+++ models/Song.js
@@ -1,7 +1,12 @@
+const { DataTypes } = require('sequelize');
 const {Sequelize, sequelize, Model} = require('../db');
 
 // TODO - define the Song model
-let Song;
+let Song = sequelize.define("song", {
+    title: DataTypes.STRING,
+    year: DataTypes.NUMBER,
+    length: DataTypes.NUMBER
+})
 
 module.exports = {
     Song
