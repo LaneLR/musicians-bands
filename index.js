a34a213cdd2f5143e91c4ef63c3b5398e250cf99
Emmanuel Luis <Emmanuel.luis@verizon.com>
2025-05-15 14:09:13 -0500

    Implemented Manager Model and Added Tests

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { Band } = require('./models/Band')
 const { Musician } = require('./models/Musician')
 const { Song } = require("./models/Song")
+const { Manager } = require("./models/Manager");
 // Define associations here
 
 Band.hasMany(Musician);
@@ -9,8 +10,12 @@ Musician.belongsTo(Band);
 Song.belongsToMany(Band, {through:"songband"})
 Band.belongsToMany(Song, {through: "songband"})
 
+Band.hasOne(Manager);
+Manager.belongsTo(Band);
+
 module.exports = {
     Band,
     Musician,
-    Song
+    Song,
+    Manager,
 };
